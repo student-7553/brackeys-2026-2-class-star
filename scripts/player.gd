46ab0175f0b74464ad48398eb725622812a0ea01
Bilguun <soproatlol@gmail.com>
2026-08-23 21:30:08 +1000

    Added the option for player to jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @export var SPEED := 5.0
+@export var JUMP_VELOCITY := 4.5
 @export var MOUSE_SENSITIVITY := 0.002
 
 @onready var camera: Camera3D = $Camera3D
@@ -26,6 +27,9 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
